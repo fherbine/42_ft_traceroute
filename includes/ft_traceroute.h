3dd844c971b44f6d77c89313dcdd8943f5a01daf
fherbine <felix.herbinet@yahoo.com>
2021-11-25 13:10:20 +0100

    nslookup

--- includes/ft_traceroute.h
+++ includes/ft_traceroute.h
@@ -7,11 +7,30 @@
 /* printf funcs */
 # include <stdio.h>
 
+/* addrinfo */
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netdb.h>
+
 /* exit */
 # include <stdlib.h>
 
 /* defines & macros */
 # define __NOP__(x) (void)x;
+
+/* typedefs */
+typedef struct addrinfo t_addrinfo;
+typedef struct sockaddr t_sockaddr;
+typedef struct sockaddr_in t_sockaddr_in;
+typedef struct sockaddr_in6 t_sockaddr_in6;
+typedef struct msghdr t_msghdr;
+
+typedef struct	s_nameinfo
+{
+	char		server[NI_MAXSERV];
+	char		host[NI_MAXHOST];
+}				t_nameinfo;
+
 
 /* prototypes */
 
@@ -22,6 +41,8 @@ void		message_description_exit(char *identifier, char *msg, int status);
 void		invalid_option(char option_char);
 void		message_exit(char *msg, int status, uint8_t with_help);
 
-
+/* nslookup.c */
+void				dnslookup(char *host, t_sockaddr *addr, uint8_t ipver);
+void				reversedns4(t_sockaddr_in *addr, t_nameinfo *info);
 
 #endif
---
+++ sources/nslookup.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nslookup.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fherbine <fherbine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/29 16:55:25 by fherbine          #+#    #+#             */
+/*   Updated: 2020/12/29 16:55:25 by fherbine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_traceroute.h"
+
+
+void				dnslookup(char *host, t_sockaddr *addr, uint8_t ipver)
+{
+	t_addrinfo		hints;
+	t_addrinfo		*first_info;
+
+	ft_bzero(&hints, sizeof(t_addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags |= AI_CANONNAME;
+	if (getaddrinfo(host, NULL, &hints, &first_info))
+		message_description_exit(host, \
+								 "Name or service not known", EXIT_FAILURE);
+	
+	if (!first_info)
+		exit(EXIT_FAILURE);
+
+	t_addrinfo *p = NULL;
+
+	p = first_info;
+	while (p)
+	{
+		if (ipver == AF_INET)
+			addr = ((t_sockaddr *)p->ai_addr);
+		else if (ipver == AF_INET6)
+			addr = ((t_sockaddr *)p->ai_addr);
+
+		p = p->ai_next;
+	}
+	freeaddrinfo(first_info);
+}
+
+void			reversedns4(t_sockaddr_in *addr, t_nameinfo *info)
+{
+	ft_bzero(info, sizeof(t_nameinfo));
+	ft_bzero(info->host, NI_MAXHOST);
+	if (getnameinfo((t_sockaddr *)addr, sizeof(t_sockaddr), info->host, NI_MAXHOST, NULL, 0, 0))
+		exit(EXIT_FAILURE);
+}
