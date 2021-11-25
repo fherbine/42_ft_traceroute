b7ec9148a1d2db6dd3b583f57fd751f4b9dcbead
fherbine <felix.herbinet@yahoo.com>
2021-11-25 13:09:00 +0100

    error.c  from ft_ping

--- includes/ft_traceroute.h
+++ includes/ft_traceroute.h
@@ -1,8 +1,27 @@
 #ifndef FT_TRACEROUTE_H
 # define FT_TRACEROUTE_H
 
+/* libft */
+# include "../libft/libft.h"
+
 /* printf funcs */
 # include <stdio.h>
+
+/* exit */
+# include <stdlib.h>
+
+/* defines & macros */
+# define __NOP__(x) (void)x;
+
+/* prototypes */
+
+/** error.c **/
+void		proper_exit(int status);
+void		display_help(int status);
+void		message_description_exit(char *identifier, char *msg, int status);
+void		invalid_option(char option_char);
+void		message_exit(char *msg, int status, uint8_t with_help);
+
 
 
 #endif
---
+++ sources/error.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fherbine <fherbine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/30 19:50:18 by fherbine          #+#    #+#             */
+/*   Updated: 2020/12/30 19:50:18 by fherbine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_traceroute.h"
+
+static void		display_option(char *letter, char *description, char *placeholder)
+{
+	printf("  %s %-16s%s\n", letter, placeholder, description);
+}
+
+void		proper_exit(int status)
+{
+	__NOP__(status);
+}
+
+void		display_help(int status)
+{
+	printf("\nUsage\n");
+	printf("  ./ft_traceroute [options] <destination>\n");
+	printf("\nOptions:\n");
+	display_option("", "  dns name or ip address", "<destination>");
+	display_option("-I", "use ICMP probes instead of UDP (by default).", "");
+	display_option("-h", "print help and exit", "");
+	proper_exit(status);
+}
+
+void	message_description_exit(char *identifier, char *msg, int status)
+{
+	dprintf((!status) ? STDOUT : STDERR, "ft_traceroute: %s: %s\n", identifier, msg);
+	proper_exit(status);
+}
+
+void	invalid_option(char option_char)
+{
+	dprintf(STDERR, "ft_traceroute: invalid option -- '%c'\n", option_char);
+	display_help(EXIT_FAILURE);
+}
+
+void	message_exit(char *msg, int status, uint8_t with_help)
+{
+	dprintf((!status) ? STDOUT : STDERR, "ft_traceroute %s\n", msg);
+	if (with_help)
+		display_help(status);
+	proper_exit(status);
+}
